06ed34583bf9b2374d35d97ff415a31899f5192c
sccsc1342025 <sebastiancam711@gmail.com>
2025-03-06 21:53:14 +0000

    csc134_M3LAB2.cpp

--- csc134_M3LAB2.cpp
+++ csc134_M3LAB2.cpp
@@ -4,12 +4,32 @@
 // 03/06/2025
 
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    //
+int main() {
+    // variables
+    int numericalGrade;
+
+    // user input
+    cout << "Enter the numerical grade (0-100): ";
+    cin >> numericalGrade;
+
+    // conditional statements
+    if (numericalGrade < 0 || numericalGrade > 100) {
+        cout << "Invalid input! Please enter a grade between 0 and 100." << endl;
+    }
+
+    if (numericalGrade >= 90) {
+        cout << "Your letter grade is: A" << endl;
+    } else if (numericalGrade >= 80) {
+        cout << "Your letter grade is:B" << endl;
+    } else if (numericalGrade >= 70) {
+        cout << "Your letter grade is: C" << endl;
+    } else if (numericalGrade >= 60) {
+        cout << "Your letter grade is: D" << endl;
+    } else {
+        cout << "Your letter grade is: F" << endl;
+    }
 
     return 0;
 }
